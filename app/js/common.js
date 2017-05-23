81f15e7bb98da3d786ba6bda62a39bfefa220cd1
elnasim <archlich21@yandex.ru>
2017-05-23 20:18:14 +0300

    add work

---
+++ app/js/common.js
@@ -0,0 +1,41 @@
+$(function() {
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+
+	//counter
+	$('.clock').FlipClock(100000,{
+		clockFace: 'DailyCounter',
+		countdown: 'true',
+		language: 'ru'
+	});
+
+});
---
+++ app/mail.php
@@ -0,0 +1,59 @@
+<?php header("Content-Type: text/html; charset=utf-8");?>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<?php
+if (isset ($_POST['phone'])) {
+	$to = "Andybrown.art@gmail.com";
+	$from = $_POST['phone'];
+	$subject = "Заполнена контактная форма с ".$_SERVER['HTTP_REFERER'];
+	$message = "Имя: ".$_POST['name']."\nEmail: ".$_POST['email']."\nНомер телефона: ".$_POST['phone']."\nФорма: ".$_POST['form_name']."\nIP: ".$_SERVER['REMOTE_ADDR'];
+	$boundary = md5(date('r', time()));
+	$filesize = '';
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers .= "From: " . $from . "\r\n";
+	$headers .= "Reply-To: " . $from . "\r\n";
+	$headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+	$message="
+Content-Type: multipart/mixed; boundary=\"$boundary\"
+
+--$boundary
+Content-Type: text/plain; charset=\"utf-8\"
+Content-Transfer-Encoding: 7bit
+
+$message";
+  for($i=0;$i<count($_FILES['fileFF']['name']);$i++) {
+      if(is_uploaded_file($_FILES['fileFF']['tmp_name'][$i])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF']['tmp_name'][$i])));
+         $filename = $_FILES['fileFF']['name'][$i];
+         $filetype = $_FILES['fileFF']['type'][$i];
+         $filesize += $_FILES['fileFF']['size'][$i];
+         $message.="
+
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+
+$attachment";
+     }
+   }
+   $message.="
+--$boundary--";
+
+  if ($filesize < 10000000) {
+    mail($to, $subject, $message, $headers);
+    $output = '<script>alert("Ваше сообщение получено, спасибо!");</script>'
+    <meta http-equiv="refresh" content="0; url=http://www.адрес_сайта/страница_сайта">;
+  } else {
+    $output = '<script>alert("Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.");</script>'
+    <meta http-equiv="refresh" content="0; url=http://www.адрес_сайта/страница_сайта">;
+  }
+ 
+}
+?>
+
+<?php echo $output; 
+ header('Refresh: 1; URL=../index.html');
+ ?>
+
+
+
